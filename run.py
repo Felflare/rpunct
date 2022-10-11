6ba9f63922e0814a628328d29cab1bef7e8e2490
Tom Potter <tom.potter@bbc.co.uk>
2022-10-11 17:13:21 +0100

    refactored run file to use subparsers

--- run.py
+++ run.py
@@ -7,17 +7,14 @@ from training.prep_data import e2e_data, check_data_exists
 
 # Parser
 parser = argparse.ArgumentParser(description='Global run file to execute dataset preparation, model training, and testing.')
+subparsers = parser.add_subparsers(help="Specify which stage of the ML process to be executed: data preparation (`data`), training (`train`), or testing (`test`).", dest="subcommand")
 
-# General arguments
-parser.add_argument(
-    'stage',
-    metavar='STAGE',
-    type=str,
-    choices=['data', 'train', 'test'],
-    help="Specify which stage of the ML process to be executed: data preparation (`data`), training (`train`), or testing (`test`). N.B. required for all processes."
-)
+data_parser = subparsers.add_parser('data', help='Execute data preparation process.')
+train_parser = subparsers.add_parser('train', help='Execute model training process.')
+test_parser = subparsers.add_parser('test', help='Execute model testing process.')
 
-parser.add_argument(
+# Data, training, & testing arguments
+data_parser.add_argument(
     '-d',
     '--data',
     metavar='DATA',
@@ -27,7 +24,29 @@ parser.add_argument(
     help="Specify the dataset to be used to test the model: BBC News (`news`) or Yelp reviews (`reviews`). N.B. impacts all processes (default is BBC News)."
 )
 
-parser.add_argument(
+train_parser.add_argument(
+    '-d',
+    '--data',
+    metavar='DATA',
+    type=str,
+    choices=['news', 'reviews'],
+    default='news',
+    help="Specify the dataset to be used to test the model: BBC News (`news`) or Yelp reviews (`reviews`). N.B. impacts all processes (default is BBC News)."
+)
+
+test_parser.add_argument(
+    '-d',
+    '--data',
+    metavar='DATA',
+    type=str,
+    choices=['news', 'reviews'],
+    default='news',
+    help="Specify the dataset to be used to test the model: BBC News (`news`) or Yelp reviews (`reviews`). N.B. impacts all processes (default is BBC News)."
+)
+
+# Training & testing arguments
+
+train_parser.add_argument(
     '-c',
     '--cuda',
     action='store_true',
@@ -35,7 +54,23 @@ parser.add_argument(
     help="Toggle between training on a GPU using CUDA or on the CPU. N.B. impacts training and testing (default is CPU)."
 )
 
-parser.add_argument(
+test_parser.add_argument(
+    '-c',
+    '--cuda',
+    action='store_true',
+    default=False,
+    help="Toggle between training on a GPU using CUDA or on the CPU. N.B. impacts training and testing (default is CPU)."
+)
+
+train_parser.add_argument(
+    '-s',
+    '--stats',
+    action='store_true',
+    default=False,
+    help="Print label distribution statistics about the test dataset. N.B. impacts training and testing (default hides stats)."
+)
+
+test_parser.add_argument(
     '-s',
     '--stats',
     action='store_true',
@@ -44,7 +79,7 @@ parser.add_argument(
 )
 
 # Training arguments
-parser.add_argument(
+train_parser.add_argument(
     '-v',
     '--val',
     action='store_true',
@@ -52,7 +87,7 @@ parser.add_argument(
     help="Toggle validation, where the model is evaluated every 5000 steps during training. N.B. only impacts training (default is off)."
 )
 
-parser.add_argument(
+train_parser.add_argument(
     '-p',
     '--plot',
     action='store_true',
@@ -61,7 +96,7 @@ parser.add_argument(
 )
 
 # Testing arguments
-parser.add_argument(
+test_parser.add_argument(
     '-m',
     '--models',
     metavar='MODEL',
@@ -74,7 +109,9 @@ parser.add_argument(
 
 # Parse arguments
 args = parser.parse_args()
-print(f"Arguments:\n{pd.Series(vars(args))}")
+print(args)
+# print(f"Arguments:\n{pd.Series(vars(args))}")
+exit(0)
 
 if args.stage == 'data':
     # run data preparation pipeline
